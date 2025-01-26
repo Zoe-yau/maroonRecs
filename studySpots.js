c3591b49c1a2a76da5cad08b7295af23cc5d9388
Zoe-yau <zoe.yau@student.allenisd.org>
2025-01-26 00:26:26 -0600

    Back buttons

---
+++ studySpots.js
@@ -0,0 +1,10 @@
+const study = [4
+    {name: "4th floor annex", }
+    {name: "ILCB", }
+    {name: "Zach", }
+    {name: "Annex 5th floor", }
+    {name: "Cushing Library" }
+    {name: "Quad bucks study rooms" }
+    {name: "Arch A"}
+
+];
