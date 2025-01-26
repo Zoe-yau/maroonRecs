637a65d64e51d10b44948ea1d51c9c6f3695be4c
Zoe-yau <zoe.yau@student.allenisd.org>
2025-01-26 02:04:26 -0600

    Delete

--- studyspots.js
+++ studyspots.js
@@ -1,10 +1,20 @@
 const study = [
-    {name: "4th floor annex", },
-    {name: "ILCB", },
-    {name: "Zach", },
-    {name: "Annex 5th floor", },
-    {name: "Cushing Library" },
-    {name: "Quad bucks study rooms" },
-    {name: "Arch A"},
+    {name: "4th floor annex",location: "Central", weekdayhours: "7 AM - 3 AM", weekendHours: "9 AM - 7 PM"},
+    {name: "ILCB", location: "Southside", Sunday: "Closed", Monday: "7:15 AM - 8 PM", Tuesday: "7:15 AM-8 PM", Wednesday: "7:15 AM- 8 PM", Thursday: "7:15 AM - 8 PM", Friday: "7 AM - 5 PM", Saturday:	"Closed"},
+    {name: "Zach", location: "Northside", weekdayhours: "", weekendHours: ""},
+    {name: "Annex 5th floor", location: "Central", weekdayhours: "7 AM - 3 AM", weekendHours: "9 AM - 7 PM"},
+    {name: "Cushing Library", location: "Central", weekdayhours: "", weekendHours: ""},
+    {name: "Quad bucks study rooms", location: "Southside", weekdayhours: "", weekendHours: ""},
+    {name: "Arch A", locaiton: "Northside", weekdayhours: "", weekendHours: ""},
+    {name: "Student Computing Center", location: "Southside", weekdayhours: "", weekendHours: ""}}
 
 ];
+
+
+Sunday	Closed
+Monday	8 AM–5 PM
+Tuesday	8 AM–5 PM
+Wednesday	8 AM–5 PM
+Thursday	8 AM–5 PM
+Friday	8 AM–5 PM
+Saturday	Closed
